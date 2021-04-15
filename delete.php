41e31f7052ba1aa894848f44937e77c0730abf49
Anmol Sharma <anmolsh.1908@gmail.com>
2021-04-15 20:13:09 +0530

    made delete.php to work

--- delete.php
+++ delete.php
@@ -3,17 +3,11 @@
 require_once("config.php");
  
 //get id of the record from url using supergloabl variable $_GET['id']
-$id = $_POST['id'];
+$id = $_GET['id'];
  
 //deleting the row from table
-$sql = "DELETE * FROM users WHERE id = '$id'";
+$sql = "DELETE FROM users WHERE Id = '$id'";
 $result = $mysqli->query($sql);
-if($result){
-    echo "deleted successfully";
-}
-else{
-    echo "Unable to delete";
-}
 //redirecting to the home page (index.php in our case)
-header("LOCATION : index.php");
+header("LOCATION: index.php");
 ?>
